01979120586edf985acdb4aad38c02f0481d7890
Azikenp <azikenp26@gmail.com>
2025-03-08 21:33:35 +0100

    getsalesdata()

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -5,12 +5,30 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import db from "@/db/db";
 import React from "react";
 
-const AdminDashboard = () => {
+async function getSalesData() {
+  const data = await db?.order.aggregate({
+    _sum: { pricePaidInCents: true },
+    _count: true,
+  });
+
+  return {
+    amount: (data._sum.pricePaidInCents || 0) / 100,
+    numberOfSales: data._count,
+  };
+}
+
+const AdminDashboard = async () => {
+  const salesData = await getSalesData();
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-      <DashboardCard title="Sales" subtitle="text" body="body" />
+      <DashboardCard
+        title="Sales"
+        subtitle={salesData.numberOfSales}
+        body={salesData.amount}
+      />
     </div>
   );
 };
