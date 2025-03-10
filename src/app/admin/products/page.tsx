a3bc4888987e7e107af295544120c4deb133bcbf
Azikenp <azikenp26@gmail.com>
2025-03-10 21:22:34 +0100

    shadcn table

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -1,10 +1,15 @@
+import { Button } from "@/components/ui/button";
 import PageHeader from "../_components/PageHeader";
+import Link from "next/link";
 
 const page = () => {
   return (
     <>
       <div className="flex justify-between items-center gap-4">
         <PageHeader>Products</PageHeader>
+        <Button>
+          <Link href="/admin/products/new">Add Products</Link>
+        </Button>
       </div>
     </>
   );
