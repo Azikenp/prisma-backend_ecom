f626cf4c3113870ca6b6167f3fc3209a581b2162
Azikenp <azikenp26@gmail.com>
2025-03-10 21:32:02 +0100

    product form

--- src/app/admin/products/new/page.tsx
+++ src/app/admin/products/new/page.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import PageHeader from "../../_components/PageHeader";
+import ProductForm from "../_components/ProductForm";
 
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <>
+      <PageHeader>Add Product</PageHeader>
+      <ProductForm />
+    </>
+  );
+};
 
-export default page
+export default page;
--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/ui/button";
 import PageHeader from "../_components/PageHeader";
 import Link from "next/link";
-import { Table, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { Table, TableBody, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 
 const page = () => {
   return (
@@ -35,6 +35,9 @@ function ProductsTable() {
           </TableHead>
         </TableRow>
       </TableHeader>
+      <TableBody>
+        
+      </TableBody>
     </Table>
   );
 }
