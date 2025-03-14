c6d57119e1393754a3dfe1bbedb3ff9016437ebd
Azikenp <azikenp26@gmail.com>
2025-03-14 22:38:15 +0100

    using client

--- src/app/admin/products/_components/ProductActions.tsx
+++ src/app/admin/products/_components/ProductActions.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { DropdownMenu } from "@/components/ui/dropdown-menu";
 import { useTransition } from "react";
 import { deleteProduct, toggleProductAvailability } from "../../_components/_actions/products";
