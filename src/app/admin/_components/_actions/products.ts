f9e5f5b5a3fe6781800344420f6c09ca23099f56
Azikenp <azikenp26@gmail.com>
2025-03-15 15:34:30 +0100

    delte imagePath and filePath

--- src/app/admin/_components/_actions/products.ts
+++ src/app/admin/_components/_actions/products.ts
@@ -60,7 +60,10 @@ export async function toggleProductAvailability(
 }
 
 export async function deleteProduct(id: string) {
-  const product  = await db.product.delete({ where: { id } });
+  const product = await db.product.delete({ where: { id } });
 
-  if(product == null ) return notFound()
+  if (product == null) return notFound();
+
+  await fs.unlink(product.filePath);
+  await fs.unlink(`public${product.imagePath}`);
 }
