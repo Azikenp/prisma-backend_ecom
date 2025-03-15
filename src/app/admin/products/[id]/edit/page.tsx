692d6ec457f57638052f58eaabc1b3aedc553d65
Azikenp <azikenp26@gmail.com>
2025-03-15 16:07:39 +0100

    edit page

---
+++ src/app/admin/products/[id]/edit/page.tsx
@@ -0,0 +1,13 @@
+import PageHeader from "@/app/admin/_components/PageHeader";
+import ProductForm from "../../_components/ProductForm";
+
+const EditProductPage = () => {
+  return (
+    <>
+      <PageHeader>Edit Product</PageHeader>
+      <ProductForm />
+    </>
+  );
+};
+
+export default page;
