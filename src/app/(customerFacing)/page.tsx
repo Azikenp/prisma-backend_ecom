fbc167ac58e898d66eaac91259049ed6d0208a60
Azikenp <azikenp26@gmail.com>
2025-03-16 22:12:25 +0100

    Products cards added

--- src/app/(customerFacing)/page.tsx
+++ src/app/(customerFacing)/page.tsx
@@ -54,6 +54,10 @@ function ProductGridSection({
           </Link>
         </Button>
       </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <ProductCard />
+      </div>
     </div>
   );
 }
