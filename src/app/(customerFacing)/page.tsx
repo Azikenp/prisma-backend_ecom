9e02342700fd7eb5b94cabb51adda0cb1b89b4b2
Azikenp <azikenp26@gmail.com>
2025-03-16 21:55:58 +0100

    get newest productss func

--- src/app/(customerFacing)/page.tsx
+++ src/app/(customerFacing)/page.tsx
@@ -1,4 +1,14 @@
-function getNewestProducts(){}
+import db from "@/db/db";
+
+function getNewestProducts() {
+  return db.product.findMany({
+    where: { isAvailableForPurchase: true },
+    orderBy: { orders: { _count: "desc" } },
+    take: 6,
+  });
+
+}
+
 
 const HomePage = () => {
   return <h1>Hi</h1>;
