0c369623f0dc7f05e5345e68653642b68e27cab5
Azikenp <azikenp26@gmail.com>
2025-03-16 15:13:45 +0100

    unlinkin

--- src/app/admin/products/_components/ProductForm.tsx
+++ src/app/admin/products/_components/ProductForm.tsx
@@ -9,6 +9,7 @@ import { useState } from "react";
 import { addProduct } from "../../_components/_actions/products";
 import { useFormState, useFormStatus } from "react-dom";
 import { Product } from "@prisma/client";
+import Image from "next/image";
 
 const ProductForm = ({ product }: { product?: Product | null }) => {
   const [priceInCents, setPriceInCents] = useState<number | undefined>(
@@ -21,7 +22,13 @@ const ProductForm = ({ product }: { product?: Product | null }) => {
       {" "}
       <div className="space-y-2">
         <Label htmlFor="name">Name</Label>
-        <Input type="text" id="name" name="name" required />
+        <Input
+          type="text"
+          id="name"
+          name="name"
+          required
+          defaultValue={product?.name || ""}
+        />
         {error.name && <div className="text-destructive">{error.name}</div>}
       </div>
       <div className="space-y-2">
@@ -43,19 +50,35 @@ const ProductForm = ({ product }: { product?: Product | null }) => {
       </div>
       <div className="space-y-2">
         <Label htmlFor="description">Description</Label>
-        <Textarea id="description" name="description" required />
+        <Textarea
+          id="description"
+          name="description"
+          required
+          defaultValue={product?.description}
+        />
         {error.description && (
           <div className="text-destructive">{error.description}</div>
         )}
       </div>
       <div className="space-y-2">
         <Label htmlFor="file">File</Label>
-        <Input type="file" id="file" name="file" required />
+        <Input type="file" id="file" name="file" required={product == null} />
+        {product !== null && (
+          <div className="text-muted-foreground">{product?.filePath}</div>
+        )}
         {error.file && <div className="text-destructive">{error.file}</div>}
       </div>
       <div className="space-y-2">
         <Label htmlFor="image">Image</Label>
-        <Input type="file" id="image" name="image" required />
+        <Input type="file" id="image" name="image" required={product == null} />
+        {product !== null && (
+          <Image
+            src={product?.imagePath}
+            width={400}
+            height={400}
+            alt="product-image"
+          />
+        )}
         {error.image && <div className="text-destructive">{error.image}</div>}
       </div>
       <SubmitButton />
