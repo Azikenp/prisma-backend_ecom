c92a09749286895abdcf220cfec7c67a723b4eb6
Azikenp <azikenp26@gmail.com>
2025-03-16 18:00:41 +0100

    admin page 70% done

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
+import { isValidPassword } from "./lib/isValidPassword";
 
 export async function middleware(req: NextRequest) {
   if ((await isAuthenticated(req)) === false) {
@@ -15,7 +16,17 @@ async function isAuthenticated(req: NextRequest) {
 
   if (authHeader == null) return false;
 
-  const [username, password] = Buffer.from(authHeader.split(" ")[1], "base64");
+  const [username, password] = Buffer.from(authHeader.split(" ")[1], "base64")
+    .toString()
+    .split(":");
+    
+  return (
+    username === process.env.ADMIN_USERNAME &&
+    (await isValidPassword(
+      password,
+      process.env.HASHED_ADMIN_PASSWORD as string
+    ))
+  );
 }
 
 export const config = {
