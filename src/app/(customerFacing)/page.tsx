939820f4f836cd6c302d3237b46e1066fb3e4481
Azikenp <azikenp26@gmail.com>
2025-03-16 22:27:25 +0100

    rendered products on user screen

--- src/app/(customerFacing)/page.tsx
+++ src/app/(customerFacing)/page.tsx
@@ -40,7 +40,7 @@ type ProductGridSectionProps = {
   productsFetcher: () => Promise<Product[]>;
 };
 
-function ProductGridSection({
+async function ProductGridSection({
   productsFetcher,
   title,
 }: ProductGridSectionProps) {
@@ -57,7 +57,9 @@ function ProductGridSection({
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        <ProductCard />
+        {(await productsFetcher()).map((product) => (
+          <ProductCard key={product.id} {...product} />
+        ))}
       </div>
     </div>
   );
