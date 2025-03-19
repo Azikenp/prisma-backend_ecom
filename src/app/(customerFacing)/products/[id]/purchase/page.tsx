610a1f56a8bfc8d2a942d5b5b123f52f5da4e2a6
Azikenp <azikenp26@gmail.com>
2025-03-19 13:05:29 +0100

    feat: integrate Stripe payment intent creation in purchase page

--- src/app/(customerFacing)/products/[id]/purchase/page.tsx
+++ src/app/(customerFacing)/products/[id]/purchase/page.tsx
@@ -1,12 +1,24 @@
 import db from "@/db/db";
 import { notFound } from "next/navigation";
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
 
 const PurchasePage = async ({ params: { id } }: { params: { id: string } }) => {
   const product = await db.product.findUnique({ where: { id } });
 
   if (product === null) return notFound();
-  
-  return <div>PurchasePage</div>;
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: product.priceInCents,
+    currency: "usd",
+    metadata: { productId: product.id },
+  });
+  if (paymentIntent.client_secret === null) {
+    throw new Error("Stripe failed to create payment intent");
+  }
+
+  return <div>Purchase Page</div>;
 };
 
 export default PurchasePage;
