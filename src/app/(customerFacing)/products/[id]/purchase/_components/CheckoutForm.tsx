818ee476e0304bd72038d8c431282ddb003e2186
Azikenp <azikenp26@gmail.com>
2025-03-19 14:49:37 +0100

    feat: implement Stripe Elements in CheckoutForm for payment processing

--- src/app/(customerFacing)/products/[id]/purchase/_components/CheckoutForm.tsx
+++ src/app/(customerFacing)/products/[id]/purchase/_components/CheckoutForm.tsx
@@ -1,10 +1,33 @@
+"use client"
+
+import {
+  Elements,
+  PaymentElement,
+  useElements,
+  useStripe,
+} from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+
 type CheckoutFormProps = {
-  product: {};
+  product: { id: string };
   clientSecret: string;
 };
 
+const stripe = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY as string);
+
 const CheckoutForm = ({ product, clientSecret }: CheckoutFormProps) => {
-  return <h1>form</h1>;
+  return (
+    <Elements options={{ clientSecret }} stripe={stripe}>
+      <Form />
+    </Elements>
+  );
 };
 
 export default CheckoutForm;
+
+function Form() {
+  const stripe = useStripe();
+  const elements = useElements();
+
+  return <PaymentElement />;
+}
